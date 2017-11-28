63b269d12945b5a416d1451f1d12a62972ca98b8
AdamV97 <33398795+AdamV97@users.noreply.github.com>
2017-11-28 17:09:49 +0100

    Primary key
    Added primary keys.

--- skladiste.sql
+++ skladiste.sql
@@ -3,71 +3,44 @@ create database skladistenje;
 use skladistenje;
 
 create table trgovina(
-id int,
-ime varchar(50),
+id int not null primary key auto_increment,
+ime varchar(50) not null,
 adresa varchar(50),
 telefon varchar(20)
 );
 
 create table skladiste(
-id int,
+id int not null primary key auto_increment,
 povrsina varchar(10),
 adresa varchar(50),
 trgovina int
 );
 
 create table polica(
-id int,
+id int not null primary key auto_increment,
 duzina varchar(50),
 sirina varchar(50),
 visina varchar(50),
-oznaka varchar(10),
+oznaka varchar(10) not null,
 red int
 );
 
 create table red(
-id int,
+id int not null primary key auto_increment,
 duzina varchar(50),
 visina varchar(50),
 sirina varchar(50),
-oznaka varchar(10),
+oznaka varchar(10) not null,
 skladiste int
 );
 
 create table roba(
-id int,
-masa varchar(10),
+id int not null primary key auto_increment,
+masa varchar(10) not null,
 sirina varchar(50),
 visina varchar(50),
 duzina varchar(50),
-oznaka varchar(10),
-vrijednost varchar(50),
+oznaka varchar(10) not null,
+vrijednost varchar(50) not null,
 polica int
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
